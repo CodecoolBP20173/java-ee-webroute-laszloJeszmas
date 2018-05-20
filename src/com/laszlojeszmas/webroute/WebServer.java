197522cf0c511cb9939333a21c3aa2eabe1140e1
rolne <rolne1992@gmail.com>
2018-05-20 16:18:04 +0200

    Refactoring

--- src/com/laszlojeszmas/webroute/WebServer.java
+++ src/com/laszlojeszmas/webroute/WebServer.java
@@ -1,13 +1,8 @@
 package com.laszlojeszmas.webroute;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
-
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class WebServer {
@@ -24,5 +19,3 @@ public class WebServer {
         server.start();
     }
 }
-
-// TODO Another parameter for WebRoute annotation (method) - and i want to find out how to extract variables from the path
